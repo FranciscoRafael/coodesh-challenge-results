82b5bb9f5f379bd35d517d87028edd753f73b100
FranciscoRafael <franciscocapiteli14@gmail.com>
2024-09-26 12:45:42 -0300

    adding consults

---
+++ queries/insert_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO brands (brand_id, brand_name) VALUES
+(1, 'MARCA_A'),
+(2, "MARCA_B"), 
+(3, "MARCA_C");
+
+INSERT INTO categories (category_id, category_name) VALUES
+(1, 'CATEGORY_A'),
+(2, "CATEGORY_B"), 
+(3, "CATEGORY_C");
+
+INSERT INTO stores (store_id, store_name, phone, email, street, city, state, zip_code) VALUES
+(1, "NAME_A", "123-456-789", "store_a@gmail.com", "123 street", "CITY A", "STATE A", "123-123"), 
+(1, "NAME_B", "123-456-987", "store_b@gmail.com", "321 street", "CITY B", "STATE B", "123-321"),
+(1, "NAME_C", "123-654-789", "store_C@gmail.com", "567 street", "CITY C", "STATE C", "532-123");
+
+--- ... ---
